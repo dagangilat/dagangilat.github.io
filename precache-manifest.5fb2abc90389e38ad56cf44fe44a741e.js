c0ade7e156960b122a289481b9d379895f7d7605
dagangilat <dagan.eliahou.gilat@gmail.com>
2020-08-27 23:16:08 +0300

    Updates

---
+++ precache-manifest.5fb2abc90389e38ad56cf44fe44a741e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4521b5599291ec564ad2a7ee9fda32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1a5c5b1c5c6a7032c6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0db77c82ff3581fcaf22",
+    "url": "/static/js/2.b0b34b32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0b34b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1a5c5b1c5c6a7032c6",
+    "url": "/static/js/main.16c0c646.chunk.js"
+  },
+  {
+    "revision": "6ee9cc77117cd1e13c74",
+    "url": "/static/js/runtime-main.e8e873ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
